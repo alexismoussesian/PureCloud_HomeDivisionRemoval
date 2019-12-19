4851f5bd57c303ad0218649bb95ea079e8d0a383
I3DOMAIN\Alexis.Moussesian <alexis.moussesian@inin.com>
2019-12-19 18:13:11 +0100

    Add project files.

---
+++ PureCloud_HomeDivisionRemoval/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace PureCloud_HomeDivisionRemoval
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnButton2 = new System.Windows.Forms.Button();
+            this.lblEnvironment = new System.Windows.Forms.Label();
+            this.lblClientSecret = new System.Windows.Forms.Label();
+            this.lblClientId = new System.Windows.Forms.Label();
+            this.txtClientSecret = new System.Windows.Forms.TextBox();
+            this.txtClientId = new System.Windows.Forms.TextBox();
+            this.cmbEnvironment = new System.Windows.Forms.ComboBox();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.btnRemoveHomeDivision = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.btnButton2);
+            this.groupBox1.Controls.Add(this.lblEnvironment);
+            this.groupBox1.Controls.Add(this.lblClientSecret);
+            this.groupBox1.Controls.Add(this.lblClientId);
+            this.groupBox1.Controls.Add(this.txtClientSecret);
+            this.groupBox1.Controls.Add(this.txtClientId);
+            this.groupBox1.Controls.Add(this.cmbEnvironment);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 182);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PureCloud Settings";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(237, 134);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(100, 28);
+            this.btnDisconnect.TabIndex = 20;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(129, 134);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 28);
+            this.btnConnect.TabIndex = 19;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnButton2
+            // 
+            this.btnButton2.Location = new System.Drawing.Point(351, 133);
+            this.btnButton2.Name = "btnButton2";
+            this.btnButton2.Size = new System.Drawing.Size(75, 29);
+            this.btnButton2.TabIndex = 15;
+            this.btnButton2.Text = "GetOrg";
+            this.btnButton2.UseVisualStyleBackColor = true;
+            this.btnButton2.Click += new System.EventHandler(this.btnButton2_Click);
+            // 
+            // lblEnvironment
+            // 
+            this.lblEnvironment.AutoSize = true;
+            this.lblEnvironment.Location = new System.Drawing.Point(31, 41);
+            this.lblEnvironment.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEnvironment.Name = "lblEnvironment";
+            this.lblEnvironment.Size = new System.Drawing.Size(91, 17);
+            this.lblEnvironment.TabIndex = 18;
+            this.lblEnvironment.Text = "Environment:";
+            // 
+            // lblClientSecret
+            // 
+            this.lblClientSecret.AutoSize = true;
+            this.lblClientSecret.Location = new System.Drawing.Point(29, 106);
+            this.lblClientSecret.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblClientSecret.Name = "lblClientSecret";
+            this.lblClientSecret.Size = new System.Drawing.Size(92, 17);
+            this.lblClientSecret.TabIndex = 17;
+            this.lblClientSecret.Text = "Client Secret:";
+            // 
+            // lblClientId
+            // 
+            this.lblClientId.AutoSize = true;
+            this.lblClientId.Location = new System.Drawing.Point(59, 74);
+            this.lblClientId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblClientId.Name = "lblClientId";
+            this.lblClientId.Size = new System.Drawing.Size(62, 17);
+            this.lblClientId.TabIndex = 16;
+            this.lblClientId.Text = "Client Id:";
+            // 
+            // txtClientSecret
+            // 
+            this.txtClientSecret.Location = new System.Drawing.Point(129, 104);
+            this.txtClientSecret.Margin = new System.Windows.Forms.Padding(4);
+            this.txtClientSecret.Name = "txtClientSecret";
+            this.txtClientSecret.PasswordChar = '*';
+            this.txtClientSecret.Size = new System.Drawing.Size(297, 22);
+            this.txtClientSecret.TabIndex = 15;
+            // 
+            // txtClientId
+            // 
+            this.txtClientId.Location = new System.Drawing.Point(129, 72);
+            this.txtClientId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtClientId.Name = "txtClientId";
+            this.txtClientId.Size = new System.Drawing.Size(297, 22);
+            this.txtClientId.TabIndex = 14;
+            // 
+            // cmbEnvironment
+            // 
+            this.cmbEnvironment.FormattingEnabled = true;
+            this.cmbEnvironment.Items.AddRange(new object[] {
+            "mypurecloud.ie",
+            "mypurecloud.de",
+            "mypurecloud.com",
+            "mypurecloud.com.au",
+            "mypurecloud.jp"});
+            this.cmbEnvironment.Location = new System.Drawing.Point(129, 41);
+            this.cmbEnvironment.Name = "cmbEnvironment";
+            this.cmbEnvironment.Size = new System.Drawing.Size(297, 24);
+            this.cmbEnvironment.TabIndex = 13;
+            // 
+            // lstLog
+            // 
+            this.lstLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.ItemHeight = 16;
+            this.lstLog.Location = new System.Drawing.Point(13, 214);
+            this.lstLog.Margin = new System.Windows.Forms.Padding(4);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(783, 180);
+            this.lstLog.TabIndex = 17;
+            // 
+            // btnRemoveHomeDivision
+            // 
+            this.btnRemoveHomeDivision.Location = new System.Drawing.Point(532, 84);
+            this.btnRemoveHomeDivision.Name = "btnRemoveHomeDivision";
+            this.btnRemoveHomeDivision.Size = new System.Drawing.Size(195, 49);
+            this.btnRemoveHomeDivision.TabIndex = 18;
+            this.btnRemoveHomeDivision.Text = "Remove Home Division";
+            this.btnRemoveHomeDivision.UseVisualStyleBackColor = true;
+            this.btnRemoveHomeDivision.Click += new System.EventHandler(this.btnRemoveHomeDivision_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 407);
+            this.Controls.Add(this.btnRemoveHomeDivision);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "PureCloud";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnButton2;
+        private System.Windows.Forms.Label lblEnvironment;
+        private System.Windows.Forms.Label lblClientSecret;
+        private System.Windows.Forms.Label lblClientId;
+        private System.Windows.Forms.TextBox txtClientSecret;
+        private System.Windows.Forms.TextBox txtClientId;
+        private System.Windows.Forms.ComboBox cmbEnvironment;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.Button btnRemoveHomeDivision;
+    }
+}
+
---
+++ PureCloud_HomeDivisionRemoval/Form1.cs
@@ -0,0 +1,318 @@
+﻿using PureCloudPlatform.Client.V2.Api;
+using PureCloudPlatform.Client.V2.Client;
+using PureCloudPlatform.Client.V2.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace PureCloud_HomeDivisionRemoval
+{
+    public partial class MainForm : Form
+    {
+        Connection connection = null;
+        UsersApi usersApi = null;
+        AuthorizationApi authorizationApi = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            AddLog("Initialisation");
+            cmbEnvironment.SelectedIndex = 0;
+            usersApi = new UsersApi();
+            authorizationApi = new AuthorizationApi();
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Log in the GUI
+        /// </summary>
+        /// <param name="message"></param>
+        private void AddLog(string message)
+        {
+            lstLog.Items.Add($"{DateTime.Now} {message}");
+            lstLog.SelectedIndex = lstLog.Items.Count - 1;
+            lstLog.SelectedIndex = -1;
+        }
+
+        /// <summary>
+        /// Connect on PureCloud
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            connection = new Connection(lstLog, cmbEnvironment.SelectedItem.ToString(), txtClientId.Text, txtClientSecret.Text);
+            var result = connection.Connect();
+            AddLog("PureCloud connection activated: " + result);
+            connection.GetOrg();
+        }
+
+        /// <summary>
+        /// Disconnect from PureCloud
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = connection.Disconnect();
+                AddLog("PureCloud connection activated: " + result);
+            }
+            catch (Exception ex)
+            {
+                AddLog($"Error in AddRows: {ex.Message} - Not connected");
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Get the org name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnButton2_Click(object sender, EventArgs e)
+        {
+            connection.GetOrg();
+        }
+
+        /// <summary>
+        /// Get the user id from the user name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string GetUserId(string name)
+        {
+            string id = "";
+
+            try
+            {
+                UserSearchCriteria userSearchCriteria = new UserSearchCriteria();
+                userSearchCriteria.Value = name;
+
+                List<string> listOfString = new List<string>();
+                listOfString.Add("name");
+                userSearchCriteria.Fields = listOfString;
+                userSearchCriteria.Type = UserSearchCriteria.TypeEnum.Exact;
+
+                List<UserSearchCriteria> listOfUserSearchCriteria = new List<UserSearchCriteria>();
+                listOfUserSearchCriteria.Add(userSearchCriteria);
+
+                UserSearchRequest userSearchRequest = new UserSearchRequest();
+                userSearchRequest.Query = listOfUserSearchCriteria;
+
+                var userEntityListing = usersApi.PostUsersSearch(userSearchRequest);
+
+                if (userEntityListing.Total.Value.Equals(0))
+                    return "";
+
+                foreach (var user in userEntityListing.Results)
+                {
+                    if (user.Name.Equals(name))
+                    {
+                        //AddLog("GetUserId: " + user.Id);
+                        id = user.Id;
+                    }
+                }
+            }
+            catch (ApiException ex)
+            {
+                if (ex.ErrorCode != 429)
+                {
+                    AddLog($"Error in SetDefaultStation_fromCsv: {ex.Message}");
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    string ratelimitCount;
+                    string ratelimitAllowed;
+                    string ratelimitReset;
+                    ex.Headers.TryGetValue("inin-ratelimit-count", out ratelimitCount);
+                    ex.Headers.TryGetValue("inin-ratelimit-allowed", out ratelimitAllowed);
+                    ex.Headers.TryGetValue("inin-ratelimit-reset", out ratelimitReset);
+                    AddLog($"API rate limit has been reached for GetUserId, {nameof(ratelimitCount)}:{ratelimitCount}, {nameof(ratelimitAllowed)}:{ratelimitAllowed}, {nameof(ratelimitReset)}:{ratelimitReset}");
+
+                    var resetTimeSeconds = 60; // default value in case that header parsing will go wrong                
+                    AddLog($"resetTimeSeconds, {resetTimeSeconds}");
+                    int.TryParse(ratelimitReset, out resetTimeSeconds);
+                    if (resetTimeSeconds > 60) throw new Exception("API rate limit reset > 60"); // if resetTimeSeconds is grather than 60 it means that something is wrong
+
+                    AddLog($"Waiting {(resetTimeSeconds + 1)} seconds");
+                    Thread.Sleep((resetTimeSeconds + 1) * 1000);
+
+                    AddLog($"Re-calling method {nameof(GetUserId)}");
+                    id = GetUserId(name);
+                }
+            }
+            return id;
+        }
+
+        /// <summary>
+        /// Delete Home division for the list of users by looping every roles
+        /// </summary>
+        /// <param name="ListOfUser"></param>
+        /// <param name="homeDivisionId"></param>
+        public void DeleteHomeDivision(List<string> ListOfUser, string homeDivisionId)
+        {
+            int total = 0;
+
+            try
+            {
+                foreach (string user in ListOfUser)
+                {
+                    // récupérer le userid
+                    var userId = GetUserId(user);
+
+                    if (userId.Equals(""))
+                    {
+                        AddLog($"Error in DeleteHomeDivision: {user} does not exist");
+                    }
+
+                    // récupérer tous les roles du user
+                    var userRoles = usersApi.GetUserRoles(userId);
+
+                    foreach (var userRole in userRoles.Roles)
+                    {
+                        usersApi.DeleteAuthorizationSubjectDivisionRole(userId, homeDivisionId, userRole.Id);
+                    }
+
+                    AddLog($"DeleteHomeDivision succeed for user {user} #{total++}");
+                }
+            }
+            catch (ApiException ex)
+            {
+                if (ex.ErrorCode != 429)
+                {
+                    AddLog($"Error in DeleteHomeDivision: {ex.Message}");
+                    //MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    string ratelimitCount;
+                    string ratelimitAllowed;
+                    string ratelimitReset;
+                    ex.Headers.TryGetValue("inin-ratelimit-count", out ratelimitCount);
+                    ex.Headers.TryGetValue("inin-ratelimit-allowed", out ratelimitAllowed);
+                    ex.Headers.TryGetValue("inin-ratelimit-reset", out ratelimitReset);
+                    AddLog($"API rate limit has been reached, {nameof(ratelimitCount)}:{ratelimitCount}, {nameof(ratelimitAllowed)}:{ratelimitAllowed}, {nameof(ratelimitReset)}:{ratelimitReset}");
+
+                    var resetTimeSeconds = 60; // default value in case that header parsing will go wrong                
+                    AddLog($"resetTimeSeconds, {resetTimeSeconds}");
+                    int.TryParse(ratelimitReset, out resetTimeSeconds);
+                    if (resetTimeSeconds > 60) throw new Exception("API rate limit reset > 60"); // if resetTimeSeconds is grather than 60 it means that something is wrong
+
+                    AddLog($"Waiting {(resetTimeSeconds + 1)} seconds");
+                    Thread.Sleep((resetTimeSeconds + 1) * 1000);
+
+                    AddLog($"Re-calling method DeleteHomeDivision");
+                    ListOfUser.RemoveRange(0, total);
+                    DeleteHomeDivision(ListOfUser, homeDivisionId);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the division id from the division name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public string GetDivisionId(string name)
+        {
+            string id = "";
+
+            try
+            {
+                var pageNumber = 1;
+                var pageSize = 25;
+
+                var divisionEntityListing = authorizationApi.GetAuthorizationDivisions(pageSize, pageNumber, null, null, null, null, null, null, name);
+
+                foreach (var div in divisionEntityListing.Entities)
+                {
+                    if (div.Name.Equals(name))
+                    {
+                        AddLog("GetDivisionId: " + div.Id);
+                        id = div.Id;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AddLog($"Error in GetDivisionId: {ex.Message}");
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return id;
+        }
+
+        /// <summary>
+        /// Launch the deletion of the home division
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public string DeleteHomeDivision_FromCsv_withApiLimit(string filename)
+        {
+            var usersApi = new UsersApi();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filename);
+                var homeDivisionId = GetDivisionId("Home");
+                List<string> ListOfUser = new List<string>();
+
+                foreach (var user in lines)
+                {
+                    ListOfUser.Add(user);
+                }
+
+                DeleteHomeDivision(ListOfUser, homeDivisionId);
+
+            }
+            catch (ApiException ex)
+            {
+                if (ex.ErrorCode != 429)
+                {
+                    AddLog($"Error in DeleteHomeDivision: {ex.Message}");
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    string ratelimitCount;
+                    string ratelimitAllowed;
+                    string ratelimitReset;
+                    ex.Headers.TryGetValue("inin-ratelimit-count", out ratelimitCount);
+                    ex.Headers.TryGetValue("inin-ratelimit-allowed", out ratelimitAllowed);
+                    ex.Headers.TryGetValue("inin-ratelimit-reset", out ratelimitReset);
+                    AddLog($"API rate limit has been reached, {nameof(ratelimitCount)}:{ratelimitCount}, {nameof(ratelimitAllowed)}:{ratelimitAllowed}, {nameof(ratelimitReset)}:{ratelimitReset}");
+                }
+
+            }
+
+            return "";
+        }
+
+        /// <summary>
+        /// Get the filename from the explorer and launch the deletion of the home division
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRemoveHomeDivision_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "CSV Files (*.csv)|*.csv";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                DeleteHomeDivision_FromCsv_withApiLimit(dialog.FileName);
+            }
+        }
+
+
+    }
+}
